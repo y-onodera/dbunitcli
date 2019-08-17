cce3d58336f62b82a61e97621d95432d1b9544dc
y.onodera <yutaonodera0304@gmail.com>
2019-08-17 12:13:21 +0900

    workbookのcloseを明記

--- src/main/java/yo/dbunitcli/dataset/XlsDataSetWriter.java
+++ src/main/java/yo/dbunitcli/dataset/XlsDataSetWriter.java
@@ -1,19 +1,15 @@
 package yo.dbunitcli.dataset;
 
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.CellType;
-import org.apache.poi.ss.usermodel.Font;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.dbunit.dataset.DataSetException;
-import org.dbunit.dataset.DefaultDataSet;
-import org.dbunit.dataset.ITable;
-import org.dbunit.dataset.SortedTable;
+import org.apache.poi.ss.usermodel.*;
+import org.dbunit.dataset.*;
+import org.dbunit.dataset.datatype.DataType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.math.BigDecimal;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -54,6 +50,52 @@ public class XlsDataSetWriter extends org.dbunit.dataset.excel.XlsDataSetWriter 
             }
         } catch (IOException | DataSetException e) {
             throw new DataSetException(e);
+        }
+    }
+
+    @Override
+    public void write(IDataSet dataSet, OutputStream out) throws IOException, DataSetException {
+        try (Workbook workbook = this.createWorkbook()) {
+            int index = 0;
+
+            for (ITableIterator iterator = dataSet.iterator(); iterator.next(); ++index) {
+                ITable table = iterator.getTable();
+                ITableMetaData metaData = table.getTableMetaData();
+                Sheet sheet = workbook.createSheet(metaData.getTableName());
+                workbook.setSheetName(index, metaData.getTableName());
+                Row headerRow = sheet.createRow(0);
+                Column[] columns = metaData.getColumns();
+
+                int j;
+                for (j = 0; j < columns.length; ++j) {
+                    Column column = columns[j];
+                    Cell cell = headerRow.createCell(j);
+                    cell.setCellValue(column.getColumnName());
+                }
+
+                for (j = 0; j < table.getRowCount(); ++j) {
+                    Row row = sheet.createRow(j + 1);
+
+                    for (int k = 0; k < columns.length; ++k) {
+                        Column column = columns[k];
+                        Object value = table.getValue(j, column.getColumnName());
+                        if (value != null) {
+                            Cell cell = row.createCell(k);
+                            if (value instanceof Date) {
+                                this.setDateCell(cell, (Date) value, workbook);
+                            } else if (value instanceof BigDecimal) {
+                                this.setNumericCell(cell, (BigDecimal) value, workbook);
+                            } else if (value instanceof Long) {
+                                this.setDateCell(cell, new Date((Long) value), workbook);
+                            } else {
+                                cell.setCellValue(DataType.asString(value));
+                            }
+                        }
+                    }
+                }
+            }
+            workbook.write(out);
+            out.flush();
         }
     }
 
