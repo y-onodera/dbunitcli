b1299d813d45136a8cae2f01a6a78aad4173cee2
y-onodera <yutaonodera0304@gmail.com>
2020-01-25 15:16:09 +0900

    sqlserver用の設定を追加

--- src/main/java/yo/dbunitcli/application/CommandLineOption.java
+++ src/main/java/yo/dbunitcli/application/CommandLineOption.java
@@ -6,6 +6,7 @@ import org.dbunit.database.DatabaseConfig;
 import org.dbunit.database.DatabaseConnection;
 import org.dbunit.database.IDatabaseConnection;
 import org.dbunit.dataset.DataSetException;
+import org.dbunit.ext.mssql.MsSqlDataTypeFactory;
 import org.dbunit.ext.oracle.Oracle10DataTypeFactory;
 import org.dbunit.operation.CloseConnectionOperation;
 import org.dbunit.operation.DatabaseOperation;
@@ -154,15 +155,28 @@ abstract public class CommandLineOption {
         String url = this.jdbcProp.get("url").toString();
         String user = this.jdbcProp.get("user").toString();
         String pass = this.jdbcProp.get("pass").toString();
+        IDatabaseConnection result;
         try {
-            Class.forName("oracle.jdbc.driver.OracleDriver");
-            Connection conn = DriverManager.getConnection(url, user, pass);
-            IDatabaseConnection result = new DatabaseConnection(conn, user);
-            DatabaseConfig config = result.getConfig();
-            config.setProperty(DatabaseConfig.PROPERTY_DATATYPE_FACTORY, new Oracle10DataTypeFactory());
-            config.setProperty(DatabaseConfig.FEATURE_ALLOW_EMPTY_FIELDS, Boolean.TRUE);
-            config.setProperty(DatabaseConfig.FEATURE_SKIP_ORACLE_RECYCLEBIN_TABLES, Boolean.TRUE);
-            config.setProperty(DatabaseConfig.FEATURE_BATCHED_STATEMENTS, Boolean.TRUE);
+            if (url.contains("jdbc:oracle:thin")) {
+                Class.forName("oracle.jdbc.driver.OracleDriver");
+                Connection conn = DriverManager.getConnection(url, user, pass);
+                result = new DatabaseConnection(conn, user);
+                DatabaseConfig config = result.getConfig();
+                config.setProperty(DatabaseConfig.PROPERTY_DATATYPE_FACTORY, new Oracle10DataTypeFactory());
+                config.setProperty(DatabaseConfig.FEATURE_ALLOW_EMPTY_FIELDS, Boolean.TRUE);
+                config.setProperty(DatabaseConfig.FEATURE_SKIP_ORACLE_RECYCLEBIN_TABLES, Boolean.TRUE);
+                config.setProperty(DatabaseConfig.FEATURE_BATCHED_STATEMENTS, Boolean.TRUE);
+            } else if (url.contains("jdbc:sqlserver")) {
+                Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+                Connection conn = DriverManager.getConnection(url, user, pass);
+                result = new DatabaseConnection(conn, conn.getCatalog());
+                DatabaseConfig config = result.getConfig();
+                config.setProperty(DatabaseConfig.PROPERTY_DATATYPE_FACTORY, new MsSqlDataTypeFactory());
+                config.setProperty(DatabaseConfig.FEATURE_ALLOW_EMPTY_FIELDS, Boolean.TRUE);
+                config.setProperty(DatabaseConfig.FEATURE_BATCHED_STATEMENTS, Boolean.TRUE);
+            } else {
+                throw new UnsupportedOperationException("unknown url :" + url);
+            }
             return result;
         } catch (ClassNotFoundException | SQLException | DatabaseUnitException ex) {
             throw new DataSetException(ex);
