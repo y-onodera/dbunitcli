636ea11718960a2cb862cf2e2d6348944f90dfc7
y-onodera <yutaonodera0304@gmail.com>
2022-01-30 16:21:34 +0900

    selectBoxの選択に併せて必要なフォームだけを表示する制御を追加

--- gui/src/main/java/yo/dbunitcli/javafx/view/main/MainPresenter.java
+++ gui/src/main/java/yo/dbunitcli/javafx/view/main/MainPresenter.java
@@ -1,17 +1,24 @@
 package yo.dbunitcli.javafx.view.main;
 
+import com.google.common.base.Enums;
 import com.google.common.base.Strings;
+import com.google.common.collect.Maps;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import org.controlsfx.control.SearchableComboBox;
-import org.kohsuke.args4j.CmdLineParser;
-import org.kohsuke.args4j.NamedOptionDef;
-import org.kohsuke.args4j.spi.OptionHandler;
 import org.tbee.javafx.scene.layout.MigPane;
+import yo.dbunitcli.application.argument.ArgumentsParser;
 
 import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+import java.util.Map;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class MainPresenter {
 
@@ -32,6 +39,10 @@ public class MainPresenter {
     private SearchableComboBox<String> commandTypeSelect;
 
     private String selectedCommand;
+
+    private ArgumentsParser parser;
+
+    private Map<Label, Node> argument = Maps.newHashMap();
 
     @FXML
     void initialize() {
@@ -54,21 +65,55 @@ public class MainPresenter {
             return;
         }
         this.selectedCommand = this.commandTypeSelect.getSelectionModel().getSelectedItem();
-        this.clearInputFields();
         Class<?> clazz = Class.forName("yo.dbunitcli.application." + this.selectedCommand + "Option");
-        Object bean = clazz.getDeclaredConstructor().newInstance();
-        CmdLineParser parser = new CmdLineParser(bean);
+        this.parser = (ArgumentsParser) clazz.getDeclaredConstructor().newInstance();
+        this.resetInput();
+    }
+
+    private void resetInput() {
+        ArgumentsParser.OptionParam option = parser.expandOption(this.inputToArg());
+        this.clearInputFields();
         int row = 1;
-        for (OptionHandler it : parser.getOptions()) {
-            if (it.option instanceof NamedOptionDef && !it.option.isArgument()) {
-                Label label = new Label();
-                label.setText(((NamedOptionDef) it.option).name());
+        for (String key : option.keySet()) {
+            Label label = new Label();
+            label.setText(key);
+            this.commandPane.add(label, "width 150,cell 0 " + row);
+            Map.Entry<String, Class> entry = option.getColumn(key);
+            if (entry.getValue().isEnum()) {
+                ChoiceBox<String> select = new ChoiceBox<>(FXCollections.observableArrayList(
+                        Arrays.stream(entry.getValue().getEnumConstants())
+                                .map(Object::toString)
+                                .collect(Collectors.toSet()))
+                );
+                select.getSelectionModel().select(entry.getKey());
+                this.commandPane.add(select, "width 200,cell 1 " + row++);
+                this.argument.put(label, select);
+                select.getSelectionModel().selectedItemProperty().addListener((observable, newVal, oldVal) -> resetInput());
+            } else {
                 TextField text = new TextField();
-                text.setText(it.printDefaultValue());
-                this.commandPane.add(label, "width 150,cell 0 " + row);
+                text.setText(option.get(key));
                 this.commandPane.add(text, "width 200,cell 1 " + row++);
+                this.argument.put(label, text);
             }
         }
+    }
+
+    private Map<String, String> inputToArg() {
+        return this.argument.entrySet()
+                .stream()
+                .filter(it -> {
+                    if (it.getValue() instanceof TextField) {
+                        return !Strings.isNullOrEmpty(it.getKey().getText()) && !Strings.isNullOrEmpty(((TextField) it.getValue()).getText());
+                    }
+                    return !Strings.isNullOrEmpty(it.getKey().getText())
+                            && !Strings.isNullOrEmpty(((ChoiceBox) it.getValue()).getSelectionModel().getSelectedItem().toString());
+                })
+                .collect(Collectors.toMap(it -> it.getKey().getText(), it -> {
+                    if (it.getValue() instanceof TextField) {
+                        return ((TextField) it.getValue()).getText();
+                    }
+                    return ((ChoiceBox) it.getValue()).getSelectionModel().getSelectedItem().toString();
+                }));
     }
 
     private String[] commandTypes() {
@@ -77,6 +122,7 @@ public class MainPresenter {
 
     private void clearInputFields() {
         this.commandPane.getChildren().removeIf(node -> !this.commandTypeSelect.equals(node) && !this.labelSelectType.equals(node));
+        this.argument = Maps.newHashMap();
     }
 
 }
