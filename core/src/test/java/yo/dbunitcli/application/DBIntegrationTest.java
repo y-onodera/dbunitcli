85a717218e40d5cd7d759d026640542277c0be1d
y-onodera <yutaonodera0304@gmail.com>
2022-02-23 18:35:51 +0900

    postgresqlを使ったDBのテストを追加

--- core/src/test/java/yo/dbunitcli/application/DBIntegrationTest.java
+++ core/src/test/java/yo/dbunitcli/application/DBIntegrationTest.java
@@ -4,11 +4,16 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.contrib.java.lang.system.ExpectedSystemExit;
+import org.junit.experimental.categories.Category;
 import org.junit.rules.ExpectedException;
+import org.junit.runner.OrderWith;
+import org.junit.runner.manipulation.Alphanumeric;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 
+@Category(IntegrationTest.class)
+@OrderWith(Alphanumeric.class)
 public class DBIntegrationTest {
     @Rule
     public ExpectedException expectedException = ExpectedException.none();
@@ -27,16 +32,34 @@ public class DBIntegrationTest {
         this.testResourcesDir = this.baseDir.replace("target/test-classes", "src/test/resources");
     }
 
-    public void runSql() throws Exception {
-        Run.main(new String[]{"@" + this.testResourcesDir + "/paramRunSql.txt"});
+    @Test
+    public void test1_runSql() throws Exception {
+        Run.main(new String[]{"@" + this.testResourcesDir + "/paramDBIntegrationRunDdl.txt"});
     }
 
-    public void exportFromDB() throws Exception {
-        Convert.main(new String[]{"@" + this.testResourcesDir + "/paramExportFromDB.txt"});
+    @Test
+    public void test2_importToDB() throws Exception {
+        Convert.main(new String[]{"@" + this.testResourcesDir + "/paramDBIntegrationImport.txt"});
     }
 
-    public void importToDB() throws Exception {
-        Convert.main(new String[]{"@" + this.testResourcesDir + "/paramImportToDB.txt"});
+    @Test
+    public void test3_exportFromDB() throws Exception {
+        Convert.main(new String[]{"@" + this.testResourcesDir + "/paramDBIntegrationExport.txt"});
+    }
+
+    @Test
+    public void test4_runSql() throws Exception {
+        Convert.main(new String[]{"@" + this.testResourcesDir + "/paramDBIntegrationRunDml.txt"});
+    }
+
+    @Test
+    public void test5_generateSetting() throws Exception {
+        Generate.main(new String[]{"@" + this.testResourcesDir + "/paramDBIntegrationGenerateSetting.txt"});
+    }
+
+    @Test
+    public void test6_generateSql() throws Exception {
+        Generate.main(new String[]{"@" + this.testResourcesDir + "/paramDBIntegrationGenerateSql.txt"});
     }
 
 }
---
+++ core/src/test/java/yo/dbunitcli/application/IntegrationTest.java
@@ -0,0 +1,4 @@
+package yo.dbunitcli.application;
+
+public interface IntegrationTest {
+}
