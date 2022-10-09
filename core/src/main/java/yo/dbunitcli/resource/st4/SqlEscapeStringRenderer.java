717f08893800b47348ae75ceba049ec5704a9ec9
y-onodera <yutaonodera0304@gmail.com>
2022-10-09 11:25:35 +0900

    stringTemplate上でキャメルケースとスネークケースの総合変換、ST4またはjexl用に${}または$$で囲って結果を出力させられるようにformatを追加

--- core/src/main/java/yo/dbunitcli/resource/st4/SqlEscapeStringRenderer.java
+++ core/src/main/java/yo/dbunitcli/resource/st4/SqlEscapeStringRenderer.java
@@ -1,5 +1,6 @@
 package yo.dbunitcli.resource.st4;
 
+import com.google.common.base.CaseFormat;
 import org.stringtemplate.v4.StringRenderer;
 
 import java.util.Locale;
@@ -12,6 +13,18 @@ public class SqlEscapeStringRenderer extends StringRenderer {
             return o.toString();
         } else if (formatString.equals("escapeSql")) {
             return o.toString().replace("'", "''");
+        } else if (formatString.equals("LCamelToLSnake")) {
+            return CaseFormat.LOWER_CAMEL.to(CaseFormat.LOWER_UNDERSCORE, o.toString());
+        } else if (formatString.equals("LCamelToUSnake")) {
+            return CaseFormat.LOWER_CAMEL.to(CaseFormat.UPPER_UNDERSCORE, o.toString());
+        } else if (formatString.equals("LSnakeToLCamel")) {
+            return CaseFormat.LOWER_UNDERSCORE.to(CaseFormat.LOWER_CAMEL, o.toString());
+        } else if (formatString.equals("USnakeToLCamel")) {
+            return CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.LOWER_CAMEL, o.toString());
+        } else if (formatString.equals("jexlExp")) {
+            return "${" + o.toString() + "}";
+        } else if (formatString.equals("ST4Exp")) {
+            return "$" + o.toString() + "$";
         }
         return super.toString(o, formatString, locale);
     }
