10e8eaea77af440b5d96985cad7a00a32f6b7afe
y-onodera <yutaonodera0304@gmail.com>
2024-01-28 12:30:46 +0900

    materialfxバージョンアップ

--- gui/src/main/java/yo/dbunitcli/javafx/view/main/MainView.java
+++ gui/src/main/java/yo/dbunitcli/javafx/view/main/MainView.java
@@ -1,8 +1,6 @@
 package yo.dbunitcli.javafx.view.main;
 
 import com.airhacks.afterburner.views.FXMLView;
-import io.github.palexdev.materialfx.css.themes.MFXThemeManager;
-import io.github.palexdev.materialfx.css.themes.Themes;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.stage.Window;
@@ -14,7 +12,6 @@ public class MainView extends FXMLView {
 
     public void open(final Stage stage) {
         final Scene scene = new Scene(this.getView());
-        MFXThemeManager.addOn(scene, Themes.DEFAULT, Themes.LEGACY);
         stage.setScene(scene);
         stage.setResizable(true);
         stage.show();
