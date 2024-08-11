bb38e1b530de6bd52f28717f71de7df049aacfed
y-onodera <yutaonodera0304@gmail.com>
2024-08-11 11:33:10 +0900

    コマンド未入力のときにundefineが表示されないように修正

--- tauri/src/components/Header.tsx
+++ tauri/src/components/Header.tsx
@@ -9,7 +9,7 @@ export default function Header() {
             <div className="flex items-center justify-start rtl:justify-end">
               <h1>DBunit CLI</h1>
             </div>
-            <h1>{currentCommand(selected)+": "+selected.name}</h1>
+            {selected.name && <h1>{currentCommand(selected)+": "+selected.name}</h1>}
           </div>
         </div>
     );
