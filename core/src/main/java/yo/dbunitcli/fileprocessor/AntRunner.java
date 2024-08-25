876620c1174a65bba7288d520c763fb1e344b124
y-onodera <yutaonodera0304@gmail.com>
2024-08-25 18:14:53 +0900

    maven実行時にうまくいかないので、baseDirのsetをhelperのparseの後に変更

--- core/src/main/java/yo/dbunitcli/fileprocessor/AntRunner.java
+++ core/src/main/java/yo/dbunitcli/fileprocessor/AntRunner.java
@@ -9,17 +9,16 @@ import java.util.Map;
 import java.util.stream.Stream;
 
 public record AntRunner(String baseDir, String target, Map<String, Object> parameter) implements Runner {
-
     @Override
     public void runScript(final Stream<File> targetFiles) {
         targetFiles.forEach(target -> {
             final Project p = new Project();
-            p.setBasedir(this.baseDir());
             p.setUserProperty("ant.file", target.getAbsolutePath());
             p.init();
             final ProjectHelper helper = ProjectHelper.getProjectHelper();
             p.addReference("ant.projectHelper", helper);
             helper.parse(p, target);
+            p.setBasedir(this.baseDir());
             this.parameter.forEach((k, v) -> p.setProperty(k, v.toString()));
             p.executeTarget(Strings.isNotEmpty(this.target()) ? this.target() : p.getDefaultTarget());
         });
