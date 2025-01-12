3c0a0b7778890fbd720d89b723208526aedd55e8
y-onodera <yutaonodera0304@gmail.com>
2025-01-12 18:20:04 +0900

    removeのiconをremove circleに変更

--- tauri/src/components/element/Icon.tsx
+++ tauri/src/components/element/Icon.tsx
@@ -32,12 +32,13 @@ export function RemoveIcon(props: { title?: string, fill?: string }) {
         <svg
             xmlns="http://www.w3.org/2000/svg"
             height="24px"
-            viewBox="0 -960 960 960"
+            viewBox="0 0 24 24"
             width="24px"
             fill={props.fill ? props.fill : "#5f6368"}
         >
             <title>{props.title ? props.title : "remove"}</title>
-            <path d="M200-440v-80h560v80H200Z" />
+            <path d="M0 0h24v24H0z" fill="none" />
+            <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm5 11H7v-2h10v2z" />
         </svg>
     )
 }
