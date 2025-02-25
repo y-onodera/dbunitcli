42f7d064530b9bab0df15a47f55191e1f91b2029
y-onodera <yutaonodera0304@gmail.com>
2025-02-26 00:51:14 +0900

    tailwindcssをv3に戻す

--- tauri/vite.config.ts
+++ tauri/vite.config.ts
@@ -1,10 +1,9 @@
-import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-	plugins: [tailwindcss(), react()],
+	plugins: [react()],
 	build: {
 		target: "esnext",
 	},
