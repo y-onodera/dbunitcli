7f798f954f4dc9871ae7c258b3a163f985bc359b
y-onodera <yutaonodera0304@gmail.com>
2025-10-12 14:51:58 +0900

    resultSetTableはfinallyでcloseするように修正

--- core/src/main/java/yo/dbunitcli/dataset/producer/ComparableDBDataSetProducer.java
+++ core/src/main/java/yo/dbunitcli/dataset/producer/ComparableDBDataSetProducer.java
@@ -97,50 +97,53 @@ public class ComparableDBDataSetProducer implements ComparableDataSetProducer {
 
     protected void executeTable(final ITable table, final Source source) {
         try {
-            ComparableDBDataSetProducer.LOGGER.info("produce - start databaseTable={}", table.getTableMetaData().getTableName());
-            final ITableMetaData tableMetaData;
-            if (this.headerNames != null) {
-                final ITableMetaData metaData = table.getTableMetaData();
-                final Column[] columns = Arrays.stream(this.headerNames, 0, metaData.getColumns().length)
-                        .map(name -> new Column(name.trim(), DataType.UNKNOWN))
-                        .toArray(Column[]::new);
-                tableMetaData = new DefaultTableMetaData(table.getTableMetaData().getTableName()
-                        , columns
-                        , Arrays.stream(table.getTableMetaData().getPrimaryKeys())
-                        .mapToInt(column -> {
-                            try {
-                                return metaData.getColumnIndex(column.getColumnName());
-                            } catch (final DataSetException e) {
-                                throw new RuntimeException(e);
-                            }
-                        })
-                        .mapToObj(i -> columns[i])
-                        .toArray(Column[]::new));
-            } else {
-                tableMetaData = table.getTableMetaData();
-            }
-            this.consumer.startTable(source.wrap(tableMetaData));
-            if (this.loadData) {
-                final Column[] columns = table.getTableMetaData().getColumns();
-                int row = 0;
-                for (; true; row++) {
-                    try {
-                        final Object[] rows = new Object[columns.length];
-                        int columnIndex = 0;
-                        for (final Column column : columns) {
-                            rows[columnIndex++] = table.getValue(row, column.getColumnName());
-                        }
-                        this.consumer.row(rows);
-                    } catch (final RowOutOfBoundsException e) {
-                        break;
-                    }
+            try {
+                ComparableDBDataSetProducer.LOGGER.info("produce - start databaseTable={}", table.getTableMetaData().getTableName());
+                final ITableMetaData tableMetaData;
+                if (this.headerNames != null) {
+                    final ITableMetaData metaData = table.getTableMetaData();
+                    final Column[] columns = Arrays.stream(this.headerNames, 0, metaData.getColumns().length)
+                            .map(name -> new Column(name.trim(), DataType.UNKNOWN))
+                            .toArray(Column[]::new);
+                    tableMetaData = new DefaultTableMetaData(table.getTableMetaData().getTableName()
+                            , columns
+                            , Arrays.stream(table.getTableMetaData().getPrimaryKeys())
+                            .mapToInt(column -> {
+                                try {
+                                    return metaData.getColumnIndex(column.getColumnName());
+                                } catch (final DataSetException e) {
+                                    throw new RuntimeException(e);
+                                }
+                            })
+                            .mapToObj(i -> columns[i])
+                            .toArray(Column[]::new));
+                } else {
+                    tableMetaData = table.getTableMetaData();
                 }
-                ComparableDBDataSetProducer.LOGGER.info("produce - rows={}", row);
-            }
-            this.consumer.endTable();
-            ComparableDBDataSetProducer.LOGGER.info("produce - end   databaseTable={}", table.getTableMetaData().getTableName());
-            if (table instanceof final IResultSetTable resultSetTable) {
-                resultSetTable.close();
+                this.consumer.startTable(source.wrap(tableMetaData));
+                if (this.loadData) {
+                    final Column[] columns = table.getTableMetaData().getColumns();
+                    int row = 0;
+                    for (; true; row++) {
+                        try {
+                            final Object[] rows = new Object[columns.length];
+                            int columnIndex = 0;
+                            for (final Column column : columns) {
+                                rows[columnIndex++] = table.getValue(row, column.getColumnName());
+                            }
+                            this.consumer.row(rows);
+                        } catch (final RowOutOfBoundsException e) {
+                            break;
+                        }
+                    }
+                    ComparableDBDataSetProducer.LOGGER.info("produce - rows={}", row);
+                }
+                this.consumer.endTable();
+                ComparableDBDataSetProducer.LOGGER.info("produce - end   databaseTable={}", table.getTableMetaData().getTableName());
+            } finally {
+                if (table instanceof final IResultSetTable resultSetTable) {
+                    resultSetTable.close();
+                }
             }
         } catch (final DataSetException e) {
             throw new AssertionError(e);
